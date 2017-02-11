3d462cbd5d847aaa5a42298aee8da0715de359ae
Reuben Brandt <Reubennbrandt@gmail.com>
2017-02-11 13:41:45 -0800

    Add users controller logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :address,
+      :first_name,
+      :last_name,
+      :zip_code,
+      :city,
+      :state
+      )
+  end
+
 end
