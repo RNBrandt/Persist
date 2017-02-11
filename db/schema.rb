78341605914888a3b661792dad8b2c44c964f8f9
Reuben Brandt <Reubennbrandt@gmail.com>
2017-02-11 13:03:26 -0800

    Add additional columns to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170211202036) do
+ActiveRecord::Schema.define(version: 20170211205404) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -18,6 +18,9 @@ ActiveRecord::Schema.define(version: 20170211202036) do
     t.string   "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "zip_code"
+    t.string   "city"
+    t.string   "state"
   end
 
 end
