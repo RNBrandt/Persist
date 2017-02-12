ce7ea67d5cf91b74e3e3ea0b5f4374fd5d8413da
cvilla100@gmail.com <cvilla100@gmail.com>
2017-02-11 17:50:56 -0800

    Add Civic API and user views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    address = @user.address + ' ' + @user.city + ' ' + @user.state
+    address = address.split(' ').join('%20')
+    request = "https://www.googleapis.com/civicinfo/v2/representatives?key=#{ENV['GOOGLE_CIVIC_API_KEY']}  &address=#{address}"
+    response = HTTParty.get(request)
+    @user.update(civic_json: response)
+    redirect_to user_path(@user)
   end
 
   def new
