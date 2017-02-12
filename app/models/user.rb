7708593fdb019217f99f82ff9e95342f10248c49
DonLang <donaldnlang2@gmail.com>
2017-02-12 10:35:24 -0800

    Add set_civic_json method to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
   has_many :documents
-
+  before_save :set_civic_json
   validates :address, :first_name, :last_name, :zip_code, :city, :state, presence: true
+  private
+  def set_civic_json()
+    request = "https://www.googleapis.com/civicinfo/v2/representatives?key=#{ENV['GOOGLE_CIVIC_API_KEY']}  &address=#{self.address}"
+    response = HTTParty.get(request)
+    @user.update(civic_json: response)
+  end
+
 end
+
