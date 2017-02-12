fc9721c47fcf24bfe5e347e24a09a13896a56681
cvilla100@gmail.com <cvilla100@gmail.com>
2017-02-11 21:16:23 -0800

    Add seed petition

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,8 @@
 user = User.new(first_name: 'Bob', last_name: 'Ross', address: '2340 Telegraph Avenue', city: 'Berkeley', state: 'CA', zip_code: '94704')
-@user.save
+user.save
+
+client = Client.new(name: 'The Guild of Public Artists', email: 'public@art.guild', description: 'We are dedicated to enriching public life with freely available programming and instruction in the arts. We represent the interests of public television and radio artists.')
+client.save
+
+petition = Document.new(name: 'Petition to Fund Public Arts Programs in Alameda County')
+petition.save
