581662d349a778a152648b17744af0efda208e17
Reuben Brandt <Reubennbrandt@gmail.com>
2017-02-12 11:16:15 -0800

    Solve self error

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,11 @@ class User < ApplicationRecord
   before_save :set_civic_json
   validates :address, :first_name, :last_name, :zip_code, :city, :state, presence: true
   private
-  def set_civic_json()
+
+  def set_civic_json
     request = "https://www.googleapis.com/civicinfo/v2/representatives?key=#{ENV['GOOGLE_CIVIC_API_KEY']}  &address=#{self.address}"
     response = HTTParty.get(request)
-    @user.update(civic_json: response)
+    self.civic_json = response
   end
 
 end
